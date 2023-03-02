073a1c70f43862697857b3f9bc97ffea1d0a8f0a
vanessa.isabella.oliveira@gmail.com <caiqueiamatsuka@gmail.com>
2023-03-02 00:25:24 -0300

    react 1

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1>Home</h1>
+            <img src="https://mentorama.com.br/blog/wp-content/uploads/2021/12/capa-web_dev-blog-mentorama-1536x525.jpg" alt="Imagem da Tela Inicial" width="500" height="500"/>
+        </>
+    );
+}
+
+export default Home;
