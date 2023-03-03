56506de22d01f3feae203552dc9ce6211ddcd77b
vanessa.isabella.oliveira@gmail.com <caiqueiamatsuka@gmail.com>
2023-03-03 20:21:32 -0300

    route implementado

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
@@ -7,11 +8,14 @@ import './App.css';
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-       <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
       <Footer />
-    </>
+    </Router>
+
   );
 }
 
